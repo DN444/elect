d9728bbb7ce40c191666a1f940b2b5283098d399
DN444 <DipayanNandi@protonmail.com>
2024-06-15 14:59:56 +0530

    commit -m "Add a new feature"

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def election_results(y):
     plt.ylabel('Total Votes')
     plt.title('Votes Polled by the Top 5 Parties in '+y)
     plt.show()
+def winner(y):
+    x=data[data['constituency_name']==y]
+    print('The winner of the constituency is '+x['candidate_name'].iloc[0]+' of '+x['party_name'].iloc[0]+' with '+str(x['total_votes'].iloc[0])+' votes')
 root=tk.Tk()
 root.title("Get Election Results")
 root.geometry("400x200")
@@ -19,4 +22,6 @@ input_field=tk.Entry(root)
 input_field.pack()
 submit_button=tk.Button(root,text="Submit",command=lambda:election_results(input_field.get()))
 submit_button.pack()
+winner_button=tk.Button(root,text="Get Winner",command=lambda:winner(input_field.get()))
+winner_button.pack()
 root.mainloop()
